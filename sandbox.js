ed7be14368b857f541b7ee263ff8700a2d00e4b7
Seth Packham <sethpackham@seths-mbp.raleigh.ibm.com>
2017-09-01 12:51:15 -0400

    Initial commit

---
+++ sandbox.js
@@ -0,0 +1,67 @@
+/*
+open -a Google\ Chrome --args --disable-web-security --user-data-dir
+
+
+https://jsonplaceholder.typicode.com/posts/
+
+http://restedblog.herokuapp.com/seth/api/
+*/
+
+function getAllPosts() {
+
+	var allBlogs;
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        console.log("response = " + this.status);
+        console.log(this.responseText);
+        var allBlogs = JSON.parse(this.responseText);
+        
+        var myInnerHTML = "";
+
+        for (i = 0; i < 10; i++) {
+			var blogPost = allBlogs[i];
+			
+			myInnerHTML += "<article class='card'>" 
+				+ "<a href='#'>" 
+				+ "<div class='card-content'>"
+	            + "<h2>" + blogPost.title + "</h2>"
+	            + "<p>" + blogPost.body + "</p>"
+	            + "<p><a href='post.html?id=" + blogPost.id + "'>Edit</a>"
+	            + "<a href='delete.html?id=" + blogPost.id + "'>Delete</a>"
+	            + "</div>"
+	            + "<!-- .card-content -->"
+	            + "</a>"
+	            + "</article>";
+        }
+        document.getElementById("listofposts").innerHTML = myInnerHTML;
+        }
+	};
+	xmlhttp.open("GET", "https://jsonplaceholder.typicode.com/posts/", true);
+	xmlhttp.send();
+	console.log(allBlogs);
+	console.log(innerHTML);
+	return allBlogs;
+
+}
+
+function getPost(id) {
+	console.log("getBlogPost(id) starts running here");
+	
+   	var myPostTitle;
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+        	thePost = JSON.parse(this.responseText);
+        	//console.log(this.responseText);
+        	//console.log("this is thePost" + thePost);
+        	//console.log("thePost.title: " + thePost.title);
+        	myPostTitle = thePost.title;
+        	console.log(myPostTitle);
+        	return myPostTitle;
+    	}
+	};
+	xmlhttp.open("GET", "https://jsonplaceholder.typicode.com/posts/" + id, true);
+	xmlhttp.send();
+	
+}
---
+++ scripts.js
@@ -0,0 +1,92 @@
+/*Structure of data
+{
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  }
+*/
+
+//Get Posts
+function getAllPostsJQuery() {
+
+  $.getJSON( "http://restedblog.herokuapp.com/seth/api/", function( data ) {
+    var items = [];
+    $.each( data, function( key, val ) {
+      items.push( "<article class='card'><div class='card-content'>"
+      	+ "<h2>" + val.title + "</h2>"
+      	+ "<p>" + val.text + "</p><p><a href='post.html?id=" + val.id + "'>edit</a></div></article>" );
+      
+      //console.log(key + val.title);
+      console.log(JSON.stringify(val));
+    });
+ 
+    $( "<section/>", {
+      "class": "cards",
+      html: items.join( "" )
+    }).appendTo( "#main-area" );
+  });
+
+
+}
+
+function getPostJQuery() {
+
+	//	get ID parameter from URL
+	//console.log("getPostIdFromUrl starts running here");
+	postId = null;
+	var arr = document.URL.match(/id=([0-9]+)/)
+	
+	if (arr != null) {
+		postId = arr[1];
+		//console.log("the post ID in the URL = " + postId);
+		
+		$.getJSON( "http://restedblog.herokuapp.com/seth/api/" + postId, function( data ) {
+	    var items = [];
+	    myPostTitle = data.title;
+	    myPostBody = data.text;
+	    //console.log(myPostTitle);
+	 
+	    $("#posttitle").val(myPostTitle);
+	    $("#postbody").html(myPostBody);
+	//}
+  		});
+  	}
+
+}
+
+//Create and edit posts
+function createPost() {
+
+}
+
+function updatePost() {
+
+}
+
+
+//Delete posts
+function deletePost(id) {
+
+}
+
+function deleteAllPosts() {
+
+}
+
+
+
+
+
+//utilities
+function getPostIdFromUrl() {
+	console.log("getPostIdFromUrl starts running here");
+	var arr = document.URL.match(/id=([0-9]+)/)
+	postId = arr[1];
+	console.log("the post ID in the URL = " + postId);
+	return postId;
+}
+
+function getSampleData() {
+
+}
